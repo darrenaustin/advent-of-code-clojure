17086b3adfefe822c2d45a44bbc1e74fb1cd219f
Darren Austin <darren@darrenaustin.org>
2024-12-02 18:01:10 -0800

    Made 2015 1 a little cleaner.

--- src/aoc2015/day01.clj
+++ src/aoc2015/day01.clj
@@ -8,15 +8,14 @@
   (m/sum (map floor-dirs input)))
 
 (defn part2 [input]
-  ;; TODO: find a cleaner solution to this
-  (second
-   (first
-    (filter (fn [[floor _]] (< floor 0))
-            (reduce (fn [stops dir]
-                      (let [[floor idx] (peek stops)]
-                        (conj stops [(+ floor (floor-dirs dir)) (inc idx)])))
-                    [[0 0]]
-                    input)))))
+  (reduce
+   (fn [stops dir]
+     (let [[floor idx] (peek stops)]
+       (if (< floor 0)
+         (reduced idx)
+         (conj stops [(+ floor (floor-dirs dir)) (inc idx)]))))
+   [[0 0]]
+   input))
 
 (def day {:year 2015 :day 1,
           :name "Not Quite Lisp"
