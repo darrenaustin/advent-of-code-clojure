8a8b8589d56c47a6f396a982d15f8c5b05f6cc8f
Darren Austin <darren@darrenaustin.org>
2024-12-02 22:17:00 -0800

    Solved 2024 3.

---
+++ src/aoc2024/day03.clj
@@ -0,0 +1,24 @@
+(ns aoc2024.day03
+  (:require
+   [aoc.util.math :as m]
+   [aoc.util.string :as s]))
+
+(defn mult-instruction [instruction]
+  (apply * (s/parse-nums instruction)))
+
+(defn part1 [input]
+  (m/sum (map mult-instruction (re-seq #"mul\(\d+,\d+\)" input))))
+
+(defn part2 [input]
+  (loop [commands (re-seq #"mul\(\d+,\d+\)|do\(\)|don\'t\(\)" input)
+         sum 0]
+    (if-let [command (first commands)]
+      (case command
+        "do()" (recur (rest commands) sum)
+        "don't()" (recur (drop-while #(not= % "do()") commands) sum)
+        (recur (rest commands) (+ sum (mult-instruction command))))
+      sum)))
+
+(def day {:year 2024 :day 3,
+          :name "Mull It Over"
+          :part1 part1, :part2 part2})
